a38e21baaa2d475e66697a1e6e034dd8cc7073a2
Tiago <tiago_moraescem@hotmail.com>
2016-06-28 22:35:14 -0300

    Atualização 28/06 as 22:35

--- src/com/sistema/movimento/Connector.java
+++ src/com/sistema/movimento/Connector.java
@@ -1,18 +1,23 @@
 package com.sistema.movimento;
 
 import org.hibernate.Session;
-import org.jboss.jandex.Main;
+
 
 public class Connector {
+	
 	
 	public static void main(String[] args) {
 		Session sessao = null;
 		
 		try {
 			sessao = HibernateUtil.getSession().openSession();
-			System.out.println("Conectou");
+			boolean connected = sessao.isConnected();
+			
+			if(connected)
+				System.out.println("Conectou");
 			
 		}finally {
+			sessao.close();
 			System.out.println("Fechou conexão");
 		} 
 	}
