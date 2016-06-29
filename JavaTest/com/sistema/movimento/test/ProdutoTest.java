a49f32b1c817317f19b1d5c8b48ba20fdf7c1988
Tiago <tiago_moraescem@hotmail.com>
2016-06-29 12:18:36 -0300

    Atualização

--- JavaTest/com/sistema/movimento/test/ProdutoTest.java
+++ JavaTest/com/sistema/movimento/test/ProdutoTest.java
@@ -4,50 +4,85 @@ import static org.junit.Assert.*;
 
 import java.util.Date;
 
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Criterion;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.sistema.movimento.HibernateUtil;
 import com.sistema.movimento.produto.Produto;
 
+import antlr.collections.List;
+
 public class ProdutoTest {
 
-	private Session sessao;
-	private Transaction transacao;
+	private static Session sessao;
+	private static Transaction transacao;
 	
-	public void abreConexao(){
-		this.sessao = HibernateUtil.getSession().getCurrentSession();
-		this.transacao = this.sessao.beginTransaction();
+	
+	@BeforeClass
+	public static void abreConexao(){
+		sessao = HibernateUtil.getSession().getCurrentSession();
+		transacao = sessao.beginTransaction();
 		
 	}
 	
-	public void fechaConexao(){
-		this.transacao.commit();
+	@AfterClass
+	public static void fechaConexao(){
 		
 		try {
-			if(this.sessao.isOpen()){
-				this.sessao.close();
+			transacao.commit();
+		} catch (Throwable e) {
+			System.out.println("Deu problema no Commit: " + e.getMessage());
+		}finally{
+			
+			try {
+				if(sessao.isOpen()){
+					sessao.close();
+				}
+			} catch (Exception e2) {
+				System.out.println("Deu problema no fechamento da conexao : " + e2.getMessage());
 			}
-		} catch (Exception e) {
-			System.out.println("Deu problema no fechamento da conexao : " + e.getMessage());
+			
 		}
+	}
+	
+	@Before
+	public void setup(){
+		
+		Produto p1 = new Produto("lote2", "Regua", new Date(), 30, 2.5f);
+		Produto p2 = new Produto("fardo", "Papel", new Date(), 300, 1.5f);
+		Produto p3 = new Produto("edicao", "Livro", new Date(), 10, 30.0f);
+		Produto p4 = new Produto("caixa", "Caneta", new Date(), 90, 1.5f);
+		Produto p5 = new Produto("lote", "Caderno", new Date(), 50, 7.0f);
+		
+		sessao.save(p1);
+		sessao.save(p2);
+		sessao.save(p3);
+		sessao.save(p4);
+		sessao.save(p5);
 	}
 	
 	@Test
 	public void salvarProdutoTest(){
-		Produto p1 = new Produto();
-		p1.setDescricao("produto Teste");
-		p1.setEstoque(20);
-		p1.setDataCadastro(new Date());
-		p1.setUnidade("Quilo");
-		p1.setValor(1.6f);
 		
-		abreConexao();
-		this.sessao.save(p1);
-		fechaConexao();
+		String sql = "from Produto p where p.descricao like :descricao";
+		Query consulta = sessao.createQuery(sql);
+		consulta.setString("descricao", "%Re%");
+		Produto produtoPesquisa = (Produto) consulta.uniqueResult();
 		
-		assertNull(null);
+		assertEquals("lote2", produtoPesquisa.getUnidade());
+	}
+	
+	@Test
+	public void listarProdutosTest(){
+		Criteria lista = sessao.createCriteria(Produto.class);
+		List<Produto> produto = lista.list()();
 	}
 	
 }
--- src/com/sistema/movimento/produto/Produto.java
+++ src/com/sistema/movimento/produto/Produto.java
@@ -25,6 +25,18 @@ public class Produto {
 	private Integer estoque;
 	
 	private float valor;
+	
+	public Produto(){
+		
+	}
+	
+	public Produto(String unidade, String descricao, Date dataCadastro, Integer estoque, float valor) {
+		this.unidade = unidade;
+		this.descricao = descricao;
+		this.dataCadastro = dataCadastro;
+		this.estoque = estoque;
+		this.valor = valor;
+	}
 
 	public Integer getId() {
 		return id;
